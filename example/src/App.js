d7d53fe298566466be2509a1e05ecbf354048ffc
Karl J. Overå <k@rl.run>
2019-03-09 12:40:50 +0100

    Make example less ugly

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import Toggle from 'react-toggle'
+
+import 'react-toggle/style.css'
 
 import BottomScrollListener from 'react-bottom-scroll-listener'
 
@@ -7,11 +10,11 @@ export default class App extends Component {
     alertOnBottom: true,
   }
 
-  handleToggleAlertOnBottom = () => {
+  handleAlertBottomChange = () => {
     this.setState(prevState => ({ alertOnBottom: !prevState.alertOnBottom }))
   }
 
-  handleOnBottom = () => {
+  handleOnDocumentBottom = () => {
     console.log('I am at bottom! ' + Math.round(performance.now()))
 
     if (this.state.alertOnBottom) {
@@ -30,15 +33,27 @@ export default class App extends Component {
   render() {
     return (
       <div className="root">
-        <label htmlFor="alertToggle">
-          <input
-            id="alertToggle"
-            type="checkbox"
-            checked={this.state.alertOnBottom}
-            onChange={this.handleToggleAlertOnBottom}
-          />
-          <div>Show alert on bottom</div>
-        </label>
+        <header>
+          <h1>react-bottom-scroll-listener</h1>
+          <label htmlFor="alert-state">
+            <span>Use {this.state.alertOnBottom ? 'alert dialog' : 'console.log'}</span>
+            <Toggle id="alert-state" checked={this.state.alertOnBottom} onChange={this.handleAlertBottomChange} />
+          </label>
+        </header>
+
+        <div className="scrollbox">
+          <h2>Callback when document hits bottom</h2>
+          <div>Scroll down! ▼▼▼</div>
+          <div>A bit more... ▼▼</div>
+          <div>Almost there... ▼</div>
+          <div>You've reached the bottom!</div>
+        </div>
+
+        {/* When you only want to listen to the bottom of "document", you can put it anywhere */}
+        <BottomScrollListener onBottom={this.handleOnDocumentBottom} />
+
+        {/* If you want to listen for the bottom of a specific container you need to forward
+            a scrollRef as a ref to your container */}
         <BottomScrollListener onBottom={this.handleContainerOnBottom}>
           {scrollRef => (
             <div ref={scrollRef} className="innerScrollExample">
@@ -46,16 +61,9 @@ export default class App extends Component {
               <div>Scroll down! ▼▼▼</div>
               <div>A bit more... ▼▼</div>
               <div>Almost there... ▼</div>
+              <div>You've reached the bottom!</div>
             </div>
           )}
-        </BottomScrollListener>
-        <BottomScrollListener onBottom={this.handleOnBottom}>
-          <div className="scrollbox">
-            <h3>Callback when page hits bottom</h3>
-            <div>Scroll down! ▼▼▼</div>
-            <div>A bit more... ▼▼</div>
-            <div>Almost there... ▼</div>
-          </div>
         </BottomScrollListener>
       </div>
     )
