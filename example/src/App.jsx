21140d3f541ca2b7490ce665dfaf5f2cdfe0fd97
Karl J. Overå <k@rl.run>
2024-10-03 20:37:29 +0200

    chore: tailwind example.. because reasons

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import Toggle from 'react-toggle';
+import React, { useState } from 'react'
+import Toggle from 'react-toggle'
 
-import ComponentExample from './ComponentExample';
-import HookExample from './HookExample';
+import ComponentExample from './ComponentExample'
+import HookExample from './HookExample'
 
 const App = () => {
-  const [hookExample, setHookExample] = useState(true);
-  const [alertOnBottom, setAlertOnBottom] = useState(true);
+  const [hookExample, setHookExample] = useState(true)
+  const [alertOnBottom, setAlertOnBottom] = useState(true)
 
   return (
     <div>
-      <header>
-        <h1>react-bottom-scroll-listener</h1>
-        <div className="right-toggle-box">
-          <label htmlFor="alert-state">
+      <header className="bg-purple-700 p-2 flex gap-3 flex-col md:flex-row justify-between m-2 rounded text-white items-center border-2 border-purple-950">
+        <h1 className="text-xl">react-bottom-scroll-listener</h1>
+        <div className="flex gap-6">
+          <label htmlFor="alert-state" className="flex justify-center gap-3">
             <span>Use {alertOnBottom ? 'alert dialog' : 'console.log'}</span>
             <Toggle id="alert-state" checked={alertOnBottom} onChange={() => setAlertOnBottom((b) => !b)} />
           </label>
-          <label htmlFor="use-hook-state">
+          <label htmlFor="use-hook-state" className="flex justify-center gap-3">
             <span>Use {hookExample ? 'hook' : 'component'}</span>
             <Toggle id="use-hook-state" checked={hookExample} onChange={() => setHookExample((b) => !b)} />
           </label>
@@ -25,7 +25,7 @@ const App = () => {
       </header>
       {hookExample ? <HookExample alertOnBottom={alertOnBottom} /> : <ComponentExample alertOnBottom={alertOnBottom} />}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- example/src/HookExample.jsx
+++ example/src/HookExample.jsx
@@ -1,51 +1,52 @@
-import React, { useCallback } from 'react';
+import React, { useCallback } from 'react'
 
-import { useBottomScrollListener } from 'react-bottom-scroll-listener';
+import { useBottomScrollListener } from 'react-bottom-scroll-listener'
 
 const HookExample = ({ alertOnBottom }) => {
   const handleOnDocumentBottom = useCallback(() => {
-    console.log('I am at bottom! ' + Math.round(performance.now()));
+    console.log('I am at bottom! ' + Math.round(performance.now()))
 
     if (alertOnBottom) {
-      alert('Bottom hit!');
+      alert('Bottom hit!')
     }
-  }, [alertOnBottom]);
+  }, [alertOnBottom])
 
   const handleContainerOnBottom = useCallback(() => {
-    console.log('I am at bottom in optional container! ' + Math.round(performance.now()));
+    console.log('I am at bottom in optional container! ' + Math.round(performance.now()))
 
     if (alertOnBottom) {
-      alert('Bottom of this container hit!');
+      alert('Bottom of this container hit!')
     }
-  }, [alertOnBottom]);
+  }, [alertOnBottom])
 
   /* This will trigger handleOnDocumentBottom when the body of the page hits the bottom */
-  useBottomScrollListener(handleOnDocumentBottom);
+  useBottomScrollListener(handleOnDocumentBottom)
 
   /* This will trigger handleOnContainerBottom when the container that is passed the ref hits the bottom */
-  const containerRef = useBottomScrollListener(handleContainerOnBottom);
+  const containerRef = useBottomScrollListener(handleContainerOnBottom)
 
   return (
-    <div className="root">
-      <div className="scroll-box">
-        <h1>Hook example</h1>
-        <h2>Callback when document hits bottom</h2>
-        <div>Scroll down! ▼▼▼</div>
-        <div>A bit more... ▼▼</div>
-        <div>Almost there... ▼</div>
-        <div>You've reached the bottom!</div>
+    <div>
+      <div
+        ref={containerRef}
+        className="md:absolute m-8 md:m-0 top-20 right-12 h-72 md:w-60 bg-purple-200 overflow-auto p-4 border-2 border-purple-950 rounded"
+      >
+        <h4 className="font-bold">Callback when this container hits bottom</h4>
+        <div className="h-44">Scroll down! ▼▼▼</div>
+        <div className="h-44">A bit more... ▼▼</div>
+        <div className="h-44">Almost there... ▼</div>
+        <div className="h-44">You've reached the bottom!</div>
       </div>
-      <div>
-        <div ref={containerRef} className="inner-scroll-example">
-          <h4>Callback when this container hits bottom</h4>
-          <div>Scroll down! ▼▼▼</div>
-          <div>A bit more... ▼▼</div>
-          <div>Almost there... ▼</div>
-          <div>You've reached the bottom!</div>
-        </div>
+      <div className="mt-16 m-8">
+        <h1 className="text-xl font-bold">Hook example</h1>
+        <h2 className="font-bold">Callback when document hits bottom</h2>
+        <div className="h-96 border-2 border-purple-950 mt-4 p-4">Scroll down! ▼▼▼</div>
+        <div className="h-96 border-2 border-purple-950 mt-4 p-4">A bit more... ▼▼</div>
+        <div className="h-96 border-2 border-purple-950 mt-4 p-4">Almost there... ▼</div>
+        <div className="h-96 border-2 border-purple-950 mt-4 p-4">You've reached the bottom!</div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HookExample;
+export default HookExample
